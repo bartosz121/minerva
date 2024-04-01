1aa853cfe165e2d02252c38b93238006dc490d6d
bartosz121 <bmagiera121@gmail.com>
2024-04-01 21:16:14 +0200

    Use transactional session

--- backend/minerva/core/db/dependencies.py
+++ backend/minerva/core/db/dependencies.py
@@ -1,14 +1,26 @@
+from logging import getLogger
 from typing import Annotated
 
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from minerva.core.db.main import session
+from minerva.core.db.main import session as db_session
+from minerva.core.exceptions import MinervaError
+from minerva.core.repository.exceptions import RepositoryError
+from minerva.core.service.exceptions import ServiceError
+
+log = getLogger(__name__)
 
 
-async def db_session():
-    async with session() as session_:
-        yield session_
+async def get_session():
+    async with db_session() as session:
+        async with session.begin():
+            try:
+                yield session
+            except (MinervaError, RepositoryError, ServiceError) as exc:
+                log.error(str(exc))
+                await session.rollback()
+                raise exc
 
 
-DbSession = Annotated[AsyncSession, Depends(db_session)]
+DbSession = Annotated[AsyncSession, Depends(get_session)]
